3f0afcb463733c838be9d789361169aa3d159614
Manu <cabral.manuel@yandex.com>
2021-08-10 20:28:11 -0300

    fixed scripts error

--- components/Header.js
+++ components/Header.js
@@ -8,14 +8,6 @@ const Header = () => {
         name="description"
         content="Gestiona tus contraseñas de manera sencilla"
       />
-      <script
-        type="module"
-        src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
-      ></script>
-      <script
-        nomodule
-        src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
-      ></script>
     </Head>
   )
 }
