43db26662a2371fa57206ec9a1569eff5a6f5075
Manu <mcabral@frba.utn.edu.ar>
2021-08-11 21:15:38 -0300

    updated home style

--- pages/index.js
+++ pages/index.js
@@ -6,51 +6,44 @@ export default function Home() {
   const user = useUser()
 
   return (
-    <div className="container">
-      <main className="main">
-        <div>
-          <h1 className="title">Bienvenid@ a Passner!</h1>
-
-          <p className="description">
-            Te facilitamos el uso de tus contraseñas de la forma más segura en
-            un depósito cifrado en línea
-          </p>
-        </div>
-
-        <div className={styles.grid}>
-          {user === undefined && (
-            <a>
-              <h2>Cargando..</h2>
-            </a>
-          )}
-          {user === null && (
-            <>
-              <a className={styles.card}>
-                <Link href="/register">
-                  <h2>Crear cuenta</h2>
-                </Link>
-              </a>
-              <a className={styles.card}>
-                <Link href="/login">
-                  <h2>Iniciar sesión</h2>
-                </Link>
-              </a>
-            </>
-          )}
-        </div>
-        {user && (
-          <>
-            <p className="description">
-              Comienza a gestionar tus contraseñas yendo al panel de control.
-            </p>
-            <a className={styles.card}>
+    <div className="home__container">
+      {user === undefined ? (
+        <div className="spinner" />
+      ) : (
+        <>
+          <div className="container__group">
+            <h2 className="black__title">Bienvenid@ a Passner</h2>
+            {user ? (
+              <p className="black__description">
+                Comienza a gestionar tus contraseñas yendo al panel de control o
+                también puedes acceder al menu arriba a la izquierda
+              </p>
+            ) : (
+              <p className="black__description">
+                Te facilitamos el uso de tus contraseñas de la forma más segura
+                en un depósito cifrado en línea
+                <br />
+              </p>
+            )}
+          </div>
+          <div className={styles.home__buttons}>
+            {user ? (
               <Link href="/dashboard">
-                <h2>Panel de control</h2>
+                <button className={styles.btn}>Panel de control</button>
               </Link>
-            </a>
-          </>
-        )}
-      </main>
+            ) : (
+              <>
+                <Link href="/register">
+                  <button className={styles.btn}>Crear cuenta</button>
+                </Link>
+                <Link href="/login">
+                  <button className={styles.btn}>Iniciar sesión</button>
+                </Link>
+              </>
+            )}
+          </div>
+        </>
+      )}
     </div>
   )
 }
